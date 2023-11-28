205b88a24093c1b6ab7d1775e8cf6097deb99c61
hehelego <hahalegao@gmail.com>
2023-11-28 10:47:05 -0600

    start working on stage6

---
+++ stage6/parser/yywrap.c
@@ -0,0 +1,1 @@
+extern "C" int yywrap() { return 1; }
---
+++ stage6/rid.h
@@ -0,0 +1,11 @@
+#ifndef RID_H
+#define RID_H
+
+// structure declaration for a record identifier
+
+typedef struct RID {
+  int pageNo;
+  int slotNo;
+};
+
+#endif
